4706194ba9565dc40ea971aefb8d3d9eb3ed1a28
RachitSharma2001 <rachitsharma613@gmail.com>
2023-02-01 09:25:52 -0800

    Add Logical Pages

--- lstore/page.py
+++ lstore/page.py
@@ -1,4 +1,38 @@
-from .config import PHYSICAL_PAGE_SIZE, ATTRIBUTE_SIZE
+from .config import PHYSICAL_PAGE_SIZE, ATTRIBUTE_SIZE, INDIRECTION_COLUMN, INVALID_OFFSET
+
+class LogicalPage:
+
+    def __init__(self, num_cols: int):
+        self.num_cols = num_cols
+        self.phys_pages = [PhysicalPage() for _ in range(self.num_cols)]
+        self.available_chunks = [index for index in range(PhysicalPage.max_number_of_records)]
+    
+    def insert_record(self, columns: list) -> int:
+        if self.is_full():
+            return INVALID_OFFSET
+        offset = self.available_chunks.pop()
+        for ind in range(self.num_cols):
+            if columns[ind]:
+                self.phys_pages[ind].insert_value(columns[ind], offset)
+        return offset
+
+    def is_full(self) -> bool:
+        return len(self.available_chunks) == 0
+
+class BasePage(LogicalPage):
+
+    def update_indir_of_record(self, new_value: int, offset: int) -> bool:
+        phys_page_of_indir = self.phys_pages[INDIRECTION_COLUMN]
+        return phys_page_of_indir.insert_value(new_value, offset)
+
+class TailPage(LogicalPage):
+
+    def get_column_of_record(self, column_index: int, offset: int) -> int:
+        assert self.__is_valid_column_index(column_index)
+        return self.phys_pages[column_index].get_column_value(offset)
+    
+    def __is_valid_column_index(self, column_index: int) -> bool:
+        return 0 <= column_index < self.num_cols
 
 class PhysicalPage:
 
--- tests/test_page.py
+++ tests/test_page.py
@@ -1,7 +1,8 @@
 import unittest
-from lstore import PhysicalPage, PHYSICAL_PAGE_SIZE, ATTRIBUTE_SIZE
+from lstore import PhysicalPage, LogicalPage, BasePage, TailPage, ATTRIBUTE_SIZE, INDIRECTION_COLUMN, INVALID_OFFSET
 
-class TestPageMethods(unittest.TestCase):
+
+class TestPhysPage(unittest.TestCase):
 
     def test_insert_value_for_valid_offsets(self) -> None:
         page: PhysicalPage = PhysicalPage()
@@ -37,6 +38,64 @@ class TestPageMethods(unittest.TestCase):
                 second=value_to_insert,
                 msg=f"Expected: {value_to_insert} Received: {column_value}")
 
+class TestLogicalPage(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(self):
+        self.num_cols: int = 3
+        self.values_to_insert: list[int] = [9, 4, 10]
+
+    @classmethod
+    def tearDownClass(self):
+        self.num_cols: int = None
+        self.values_to_insert: list[int] = None
+
+    def test_insert_record(self) -> None:
+        page: LogicalPage = LogicalPage(self.num_cols)
+        offset = page.insert_record(self.values_to_insert)
+        assert offset != INVALID_OFFSET
+        for ind in range(self.num_cols):
+            assert page.phys_pages[ind].get_column_value(offset) == self.values_to_insert[ind]
+
+    def test_insert_record_into_full_page(self) -> None:
+        page: LogicalPage = LogicalPage(self.num_cols)
+        for _ in range(PhysicalPage.max_number_of_records):
+            page.insert_record(self.values_to_insert)
+        offset = page.insert_record(self.values_to_insert)
+        assert offset == INVALID_OFFSET
+
+    def test_is_full(self) -> None:
+        page: LogicalPage = LogicalPage(self.num_cols)
+        for _ in range(PhysicalPage.max_number_of_records):
+            assert not page.is_full()
+            page.insert_record(self.values_to_insert)
+        assert page.is_full()
+
+class TestBasePage(TestLogicalPage):
+    
+    def test_update_indir(self) -> None:
+        page: BasePage = BasePage(self.num_cols)
+        offset = page.insert_record(self.values_to_insert)
+        new_indir_val = 5
+        assert page.update_indir_of_record(new_indir_val, offset)
+        assert page.phys_pages[INDIRECTION_COLUMN].get_column_value(offset) == new_indir_val
+    
+class TestTailPage(TestLogicalPage):
+
+    def test_get_column(self):
+        page: TailPage = TailPage(self.num_cols)
+        offset = page.insert_record(self.values_to_insert)
+        for ind in range(self.num_cols):
+            assert page.get_column_of_record(ind, offset) == self.values_to_insert[ind]
+        
+    def test_get_column_invalid_index(self):
+        page: TailPage = TailPage(self.num_cols)
+        offset = page.insert_record(self.values_to_insert)
+        with self.assertRaises(AssertionError):
+            page.get_column_of_record(self.num_cols+1, offset)
+        with self.assertRaises(AssertionError):
+            page.get_column_of_record(-1, offset)
+        
 
 if __name__ == '__main__':
     unittest.main()
