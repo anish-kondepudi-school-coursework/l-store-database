ac8ce84d22687919c293cf94032676dd4b4be041
Saipranav Kotamreddy <saipranavkotamreddy@Saipranavs-MacBook-Pro.local>
2023-02-15 17:40:46 -0800

    Added some analytical tests to demonstrate scaling

--- __hundred_thousand_operations_comparison.py
+++ __hundred_thousand_operations_comparison.py
@@ -2,7 +2,7 @@ from lstore.db import Database
 from lstore.query import Query
 from time import process_time
 from random import choice, randrange
-
+print("100K")
 # Student Id and 4 grades
 db = Database()
 grades_table = db.create_table('Grades', 5, 0)
@@ -54,3 +54,113 @@ for i in range(0, 100000):
     query.delete(906659671 + i)
 delete_time_1 = process_time()
 print("Deleting 100K records took:  \t\t\t", delete_time_1 - delete_time_0)
+
+print("----------------------------------------------------------------------------------------")
+print("150K")
+
+# Student Id and 4 grades
+db = Database()
+grades_table = db.create_table('Grades', 5, 0)
+query = Query(grades_table)
+keys = []
+
+insert_time_0 = process_time()
+for i in range(0, 150000):
+    query.insert(906659671 + i, 93, 0, 0, 0)
+    keys.append(906659671 + i)
+insert_time_1 = process_time()
+
+print("Inserting 150K records took:  \t\t\t", insert_time_1 - insert_time_0)
+
+# Measuring update Performance
+update_cols = [
+    [None, None, None, None, None],
+    [None, randrange(0, 100), None, None, None],
+    [None, None, randrange(0, 100), None, None],
+    [None, None, None, randrange(0, 100), None],
+    [None, None, None, None, randrange(0, 100)],
+]
+
+update_time_0 = process_time()
+for i in range(0, 150000):
+    query.update(choice(keys), *(choice(update_cols)))
+update_time_1 = process_time()
+print("Updating 150K records took:  \t\t\t", update_time_1 - update_time_0)
+
+# Measuring Select Performance
+select_time_0 = process_time()
+for i in range(0, 150000):
+    query.select(choice(keys),0 , [1, 1, 1, 1, 1])
+select_time_1 = process_time()
+print("Selecting 150K records took:  \t\t\t", select_time_1 - select_time_0)
+
+# Measuring Aggregate Performance
+agg_time_0 = process_time()
+for i in range(0, 150000, 100):
+    start_value = 906659671 + i
+    end_value = start_value + 100
+    result = query.sum(start_value, end_value - 1, randrange(0, 5))
+agg_time_1 = process_time()
+print("Aggregate 150K of 100 record batch took:\t", agg_time_1 - agg_time_0)
+
+# Measuring Delete Performance
+delete_time_0 = process_time()
+for i in range(0, 150000):
+    query.delete(906659671 + i)
+delete_time_1 = process_time()
+print("Deleting 150K records took:  \t\t\t", delete_time_1 - delete_time_0)
+
+print("----------------------------------------------------------------------------------------")
+print("200K")
+
+# Student Id and 4 grades
+db = Database()
+grades_table = db.create_table('Grades', 5, 0)
+query = Query(grades_table)
+keys = []
+
+insert_time_0 = process_time()
+for i in range(0, 200000):
+    query.insert(906659671 + i, 93, 0, 0, 0)
+    keys.append(906659671 + i)
+insert_time_1 = process_time()
+
+print("Inserting 200K records took:  \t\t\t", insert_time_1 - insert_time_0)
+
+# Measuring update Performance
+update_cols = [
+    [None, None, None, None, None],
+    [None, randrange(0, 100), None, None, None],
+    [None, None, randrange(0, 100), None, None],
+    [None, None, None, randrange(0, 100), None],
+    [None, None, None, None, randrange(0, 100)],
+]
+
+update_time_0 = process_time()
+for i in range(0, 200000):
+    query.update(choice(keys), *(choice(update_cols)))
+update_time_1 = process_time()
+print("Updating 200K records took:  \t\t\t", update_time_1 - update_time_0)
+
+# Measuring Select Performance
+select_time_0 = process_time()
+for i in range(0, 200000):
+    query.select(choice(keys),0 , [1, 1, 1, 1, 1])
+select_time_1 = process_time()
+print("Selecting 200K records took:  \t\t\t", select_time_1 - select_time_0)
+
+# Measuring Aggregate Performance
+agg_time_0 = process_time()
+for i in range(0, 200000, 100):
+    start_value = 906659671 + i
+    end_value = start_value + 100
+    result = query.sum(start_value, end_value - 1, randrange(0, 5))
+agg_time_1 = process_time()
+print("Aggregate 200K of 100 record batch took:\t", agg_time_1 - agg_time_0)
+
+# Measuring Delete Performance
+delete_time_0 = process_time()
+for i in range(0, 200000):
+    query.delete(906659671 + i)
+delete_time_1 = process_time()
+print("Deleting 200K records took:  \t\t\t", delete_time_1 - delete_time_0)
