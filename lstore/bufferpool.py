e6063bf5b03abece5ef5f68f3661343962e181af
Anish Kondepudi <anish.kondepudi@gmail.com>
2023-02-23 23:06:28 -0800

    Fix bug in bufferpool implementation

--- lstore/bufferpool.py
+++ lstore/bufferpool.py
@@ -5,7 +5,7 @@ class Bufferpool:
 
     def __init__(self, max_buffer_pool_size: int) -> None:
         self.max_buffer_pool_size: int = max_buffer_pool_size
-        self.physical_pages: dict[PhysicalPage] = dict()
+        self.physical_pages: dict[str,PhysicalPage] = dict()
         self.disk = DiskInterface("test") # ToDo: change from "test" to real name
 
     def insert_page(self, page_id: str, slot_num: int, value: int) -> bool:
@@ -37,26 +37,27 @@ class Bufferpool:
         return physical_page
 
     def evict_all_pages(self) -> None:
-        for _ in range(self.physical_pages):
+        for _ in range(len(self.physical_pages)):
             self.__evict_page()
 
     def __evict_page(self) -> None:
         if len(self.physical_pages) == 0:
             return
 
-        physical_pages_and_page_ids: list[tuple[PhysicalPage,str]] = [(physical_page, page_id) for page_id, physical_page in self.physical_pages.items()]
-        physical_pages_and_page_ids.sort(key=lambda x: x[0].get_timestamp())
+        physical_pages_and_page_ids: list[tuple[PhysicalPage,str]] = \
+            sorted([(phys_page, page_id) for page_id, phys_page in self.physical_pages.items()],
+                   key=lambda x: x[0].get_timestamp())
 
-        idx: int = 0
+        bufferpool_page_index: int = 0
         while True:
-            physical_page, page_id = physical_pages_and_page_ids[idx]
-            idx = (idx + 1) % len(physical_pages_and_page_ids)
+            physical_page, page_id = physical_pages_and_page_ids[bufferpool_page_index]
+            bufferpool_page_index = (bufferpool_page_index + 1) % len(physical_pages_and_page_ids)
 
             if not physical_page.can_evict():
-                physical_pages_and_page_ids.append((physical_page, page_id))
                 continue
 
             if physical_page.is_dirty():
                 self.disk.write_page(page_id, physical_page)
 
             del self.physical_pages[page_id]
+            break
