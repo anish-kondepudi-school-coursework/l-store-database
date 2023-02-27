e6edcb3d0d2ef306c2addf59936c3f279bbf3c2b
RachitSharma2001 <rachitsharma613@gmail.com>
2023-02-26 17:55:06 -0800

    Modify Rid Generator to give contigious range

--- lstore/page.py
+++ lstore/page.py
@@ -6,58 +6,7 @@ from .config import (
     INVALID_SLOT_NUM,
     INVALID_RID,
 )
-from .rid import RID_Generator
-from abc import ABC, abstractmethod
 import time
-
-class LogicalPage(ABC):
-    def __init__(self, num_cols: int, rid_generator: RID_Generator):
-        self.num_cols = num_cols
-        self.phys_pages = [PhysicalPage() for _ in range(self.num_cols)]
-        self.available_chunks = [
-            index for index in range(PhysicalPage.max_number_of_records)
-        ]
-        self.rid_generator = rid_generator
-
-    def insert_record(self, columns: list):
-        if self.is_full():
-            return INVALID_RID, INVALID_SLOT_NUM
-        slot_num = self.available_chunks.pop()
-        for ind in range(self.num_cols):
-            if columns[ind] != None:
-                self.phys_pages[ind].insert_value(columns[ind], slot_num)
-        new_rid = self._generate_new_rid()
-        return new_rid, slot_num
-
-    def is_full(self) -> bool:
-        return len(self.available_chunks) == 0
-
-    def get_column_of_record(self, column_index: int, slot_num: int) -> int:
-        assert self.__is_valid_column_index(column_index)
-        return self.phys_pages[column_index].get_column_value(slot_num)
-
-    def update_indir_of_record(self, new_value: int, slot_num: int) -> bool:
-        phys_page_of_indir = self.phys_pages[INDIRECTION_COLUMN]
-        return phys_page_of_indir.insert_value(new_value, slot_num)
-
-    @abstractmethod
-    def _generate_new_rid(self):
-        pass
-
-    def __is_valid_column_index(self, column_index: int) -> bool:
-        return (0 <= column_index < self.num_cols) or (
-            column_index in (INDIRECTION_COLUMN, SCHEMA_ENCODING_COLUMN)
-        )
-
-
-class BasePage(LogicalPage):
-    def _generate_new_rid(self) -> int:
-        return self.rid_generator.new_base_rid()
-
-
-class TailPage(LogicalPage):
-    def _generate_new_rid(self) -> int:
-        return self.rid_generator.new_tail_rid()
 
 class PhysicalPage:
     max_number_of_records: int = PHYSICAL_PAGE_SIZE // ATTRIBUTE_SIZE
@@ -115,3 +64,50 @@ class PhysicalPage:
 
     def __is_slot_num_valid(self, slot_num: int) -> bool:
         return 0 <= slot_num < PhysicalPage.max_number_of_records
+
+from .rid import RID_Generator
+from abc import ABC, abstractmethod
+
+class LogicalPage(ABC):
+    def __init__(self, num_cols: int):
+        self.num_cols = num_cols
+        self.phys_pages = [PhysicalPage() for _ in range(self.num_cols)]
+        self.available_chunks = [
+            index for index in range(PhysicalPage.max_number_of_records)
+        ]
+
+    def insert_record(self, columns: list):
+        if self.is_full():
+            return INVALID_RID, INVALID_SLOT_NUM
+        slot_num = self.available_chunks.pop()
+        for ind in range(self.num_cols):
+            if columns[ind] != None:
+                self.phys_pages[ind].insert_value(columns[ind], slot_num)
+        new_rid = self.rids.pop()
+        return new_rid, slot_num
+
+    def is_full(self) -> bool:
+        return len(self.available_chunks) == 0
+
+    def get_column_of_record(self, column_index: int, slot_num: int) -> int:
+        assert self.__is_valid_column_index(column_index)
+        return self.phys_pages[column_index].get_column_value(slot_num)
+
+    def update_indir_of_record(self, new_value: int, slot_num: int) -> bool:
+        phys_page_of_indir = self.phys_pages[INDIRECTION_COLUMN]
+        return phys_page_of_indir.insert_value(new_value, slot_num)
+
+    def __is_valid_column_index(self, column_index: int) -> bool:
+        return (0 <= column_index < self.num_cols) or (
+            column_index in (INDIRECTION_COLUMN, SCHEMA_ENCODING_COLUMN)
+        )
+
+class BasePage(LogicalPage):
+    def __init__(self, num_cols: int, rid_generator: RID_Generator):
+        self.rids = rid_generator.get_base_rids()
+        super().__init__(num_cols)
+
+class TailPage(LogicalPage):
+    def __init__(self, num_cols: int, rid_generator: RID_Generator):
+        self.rids = rid_generator.get_tail_rids()
+        super().__init__(num_cols)
--- lstore/rid.py
+++ lstore/rid.py
@@ -1,14 +1,26 @@
 from .config import START_BASE_RID, START_TAIL_RID
+from .page import PhysicalPage
 class RID_Generator:
     def __init__(self):
         self.curr_base_rid = START_BASE_RID
         self.curr_tail_rid = START_TAIL_RID
 
-    def new_base_rid(self):
-        self.curr_base_rid += 1
-        return self.curr_base_rid - 1
+    def base_rid_to_starting_rid(self, base_rid) -> int:
+        max_num_records = PhysicalPage.max_number_of_records
+        return START_BASE_RID + ((base_rid-START_BASE_RID) // max_num_records) * max_num_records
+
+    def tail_rid_to_starting_rid(self, tail_rid) -> int:
+        associated_base_rid = -tail_rid
+        return -self.base_rid_to_starting_rid(associated_base_rid)
+
+    def get_base_rids(self) -> list[int]:
+        rid_high = self.curr_base_rid + PhysicalPage.max_number_of_records
+        page_rid_space = [rid for rid in range(self.curr_base_rid, rid_high)]
+        self.curr_base_rid = rid_high
+        return page_rid_space
     
-    def new_tail_rid(self):
-        self.curr_tail_rid -= 1
-        return self.curr_tail_rid + 1
-    
+    def get_tail_rids(self) -> list[int]:
+        rid_low = self.curr_tail_rid - PhysicalPage.max_number_of_records
+        page_rid_space = [rid for rid in range(rid_low+1, self.curr_tail_rid+1)]
+        self.curr_tail_rid = rid_low
+        return page_rid_space
