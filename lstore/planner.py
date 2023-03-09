39a1f4dc48c6469d055dc7b154d7eb9818a34357
Saipranav Kotamreddy <saipranavkotamreddy@Saipranavs-MacBook-Pro.local>
2023-03-09 15:01:18 -0800

    Added delete primary key grabber and fixed bug losing commands

--- lstore/planner.py
+++ lstore/planner.py
@@ -41,6 +41,10 @@ class Planner:
                     if args[0] not in self.primary_key_list:
                         self.primary_key_list[args[0]]=1
                         self.primary_key_count+=1
+                elif(query.__func__==queryObj.delete.__func__):
+                    if args[0] not in self.primary_key_list:
+                        self.primary_key_list[args[0]]=1
+                        self.primary_key_count+=1
         return self.primary_key_count
 
     def create_queues(self):
@@ -50,10 +54,30 @@ class Planner:
                 self.queue_list[primary_key + offset] = queue.Queue()
     
     def print_queues(self):
+        #Checks to make sure all transactions are queued
+        count:int = 0
+        #newList: list = []
+        test_dict = dict()
+        for i in range(1000):
+            test_dict[92106429+i]=1
         for key in self.queue_list:
-            print("Primary key: ", key)
+            #print("Primary key: ", key)
             while not self.queue_list[key].empty():
-                print(self.queue_list[key].get())
+                #print(self.queue_list[key].get())
+                value = self.queue_list[key].get()
+                count+=1
+                if key in test_dict:
+                    del test_dict[key]
+                elif key-1000 in test_dict:
+                    del test_dict[key-1000]
+                elif key-2000 in test_dict:
+                    del test_dict[key-2000]
+                elif key-3000 in test_dict:
+                    del test_dict[key-3000]
+        print(count)
+        #for key in newList:
+            #del self.queue_list[key]
+        print("Remaining: ",test_dict)
 
     def plan(self, transaction_list: list):
         #Divide transaction_list into N equal sized lists of transactions, where N = number of transactions
@@ -61,7 +85,8 @@ class Planner:
         transactions_per_priority = len(transaction_list)//self.planner_threads
         for i in range(self.planner_threads-1): 
             split_transactions.append(0)
-            split_transactions[i]=transaction_list[(i*transactions_per_priority):(((i+1)*transactions_per_priority)-1)]
+            #print((i*transactions_per_priority), (((i+1)*transactions_per_priority)-1))
+            split_transactions[i]=transaction_list[(i*transactions_per_priority):(((i+1)*transactions_per_priority))]
         split_transactions.append(0)
         split_transactions[self.planner_threads-1]=transaction_list[(self.planner_threads-1)*transactions_per_priority:]
 
@@ -89,5 +114,7 @@ class Planner:
                     if args[1]==self.table.primary_key_col:
                         primary_key=args[0]
                 elif(query.__func__==queryObj.update.__func__):
+                    primary_key=args[0]
+                elif(query.__func__==queryObj.delete.__func__):
                     primary_key=args[0]
                 self.queue_list[primary_key + offset].put((query, args))
