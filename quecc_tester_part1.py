ee550108a9fcaf68c372274b384ff65645fea7b3
RachitSharma2001 <rachitsharma613@gmail.com>
2023-03-10 17:09:47 -0800

    Add Quecc class and refactor planner class

--- quecc_tester_part1.py
+++ quecc_tester_part1.py
@@ -15,13 +15,14 @@ grades_table = db.create_table('Grades', 5, 0)
 # create a query class for the grades table
 query = Query(grades_table)
 
-planner = Planner(grades_table)
 # dictionary for records to test the database: test directory
 records = {}
 
-number_of_records = 1000
-number_of_transactions = 100
-num_threads = 8
+number_of_records = 10
+number_of_transactions = 5
+num_threads = 4
+
+planner = Planner(grades_table, num_threads)
 
 # create index on the non primary columns
 try:
@@ -42,48 +43,50 @@ for i in range(number_of_transactions):
     insert_transactions.append(Transaction())
 
 for i in range(0, number_of_records):
-    key = 92106429 + i
+    key = 92106429 + 2*i
     keys.append(key)
     records[key] = [key, randint(i * 20, (i + 1) * 20), randint(i * 20, (i + 1) * 20), randint(i * 20, (i + 1) * 20), randint(i * 20, (i + 1) * 20)]
     t = insert_transactions[i % number_of_transactions]
     t.add_query(query.insert, grades_table, *records[key])
 
-planner.find_primary_key_count(insert_transactions)
-planner.create_queues()
-queue_list = planner.plan(insert_transactions)
-planner.print_queues()
-assert(0)
-transaction_workers = []
-for i in range(num_threads):
-    transaction_workers.append(TransactionWorker())
+# combine these
+planner.plan(insert_transactions)
+# planner.find_primary_key_count(insert_transactions)
+# planner.create_queues()
+# queue_list = planner.plan(insert_transactions)
+# planner.print_queues()
+# assert(0)
+# transaction_workers = []
+# for i in range(num_threads):
+#     transaction_workers.append(TransactionWorker())
     
-for i in range(number_of_transactions):
-    transaction_workers[i % num_threads].add_transaction(insert_transactions[i])
+# for i in range(number_of_transactions):
+#     transaction_workers[i % num_threads].add_transaction(insert_transactions[i])
 
 
 
-# run transaction workers
-for i in range(num_threads):
-    transaction_workers[i].run()
+# # run transaction workers
+# for i in range(num_threads):
+#     transaction_workers[i].run()
 
-# wait for workers to finish
-for i in range(num_threads):
-    transaction_workers[i].join()
-
-
-# Check inserted records using select query in the main thread outside workers
-for key in keys:
-    record = query.select(key, 0, [1, 1, 1, 1, 1])[0]
-    error = False
-    for i, column in enumerate(record.columns):
-        if column != records[key][i]:
-            error = True
-    if error:
-        print('select error on', key, ':', record, ', correct:', records[key])
-    else:
-        pass
-        # print('select on', key, ':', record)
-print("Select finished")
+# # wait for workers to finish
+# for i in range(num_threads):
+#     transaction_workers[i].join()
 
 
-db.close()
+# # Check inserted records using select query in the main thread outside workers
+# for key in keys:
+#     record = query.select(key, 0, [1, 1, 1, 1, 1])[0]
+#     error = False
+#     for i, column in enumerate(record.columns):
+#         if column != records[key][i]:
+#             error = True
+#     if error:
+#         print('select error on', key, ':', record, ', correct:', records[key])
+#     else:
+#         pass
+#         # print('select on', key, ':', record)
+# print("Select finished")
+
+
+# db.close()
