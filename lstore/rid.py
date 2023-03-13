b13f0bdb0d377d246f928506c5aa7c48c96e34fe
Anish Kondepudi <anish.kondepudi@gmail.com>
2023-03-13 16:43:17 -0700

    Add lock for rid generator

--- lstore/rid.py
+++ lstore/rid.py
@@ -1,11 +1,14 @@
 from .config import START_BASE_RID, START_TAIL_RID
 from .phys_page import PhysicalPage
+from threading import Lock
 
 
 class RID_Generator:
     def __init__(self):
         self.curr_base_rid = START_BASE_RID
         self.curr_tail_rid = START_TAIL_RID
+        self.base_rid_lock = Lock()
+        self.tail_rid_lock = Lock()
 
     def base_rid_to_starting_rid(self, base_rid) -> int:
         max_num_records = PhysicalPage.max_number_of_records
@@ -16,16 +19,18 @@ class RID_Generator:
         return -int(self.base_rid_to_starting_rid(associated_base_rid))
 
     def get_base_rids(self) -> list[int]:
-        rid_high = self.curr_base_rid + PhysicalPage.max_number_of_records
-        page_rid_space = [rid for rid in range(self.curr_base_rid, rid_high)]
-        self.curr_base_rid = rid_high
-        return page_rid_space[::-1]
+        with self.base_rid_lock:
+            rid_high = self.curr_base_rid + PhysicalPage.max_number_of_records
+            page_rid_space = [rid for rid in range(self.curr_base_rid, rid_high)]
+            self.curr_base_rid = rid_high
+            return page_rid_space[::-1]
 
     def get_tail_rids(self) -> list[int]:
-        rid_low = self.curr_tail_rid - PhysicalPage.max_number_of_records
-        page_rid_space = [rid for rid in range(rid_low + 1, self.curr_tail_rid + 1)]
-        self.curr_tail_rid = rid_low
-        return page_rid_space
+        with self.tail_rid_lock:
+            rid_low = self.curr_tail_rid - PhysicalPage.max_number_of_records
+            page_rid_space = [rid for rid in range(rid_low + 1, self.curr_tail_rid + 1)]
+            self.curr_tail_rid = rid_low
+            return page_rid_space
 
     def get_slot_num(self, rid: int) -> int:
         assert rid != 0
