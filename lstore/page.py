bd9ddd51b78c9f7ab1d9efa5304e9d040cebecf4
Anish Kondepudi <anish.kondepudi@gmail.com>
2023-03-13 16:42:15 -0700

    Add static lock implementation for logical page

--- lstore/page.py
+++ lstore/page.py
@@ -6,14 +6,16 @@ from .config import (
     BASE_RID,
 )
 from copy import copy, deepcopy
+from threading import Lock
 from .rid import RID_Generator
 from abc import ABC
 from .bufferpool import Bufferpool
 from .phys_page import PhysicalPage
 
-
-
 class LogicalPage(ABC):
+
+    logical_page_lock = Lock()
+
     def __init__(self, table_name: str, num_cols: int, bufferpool: Bufferpool) -> None:
         self.starting_rid = self.rids[-1]
         self.num_cols = num_cols
@@ -23,25 +25,28 @@ class LogicalPage(ABC):
         self.bufferpool = bufferpool
 
     def insert_record(self, columns: list):
-        if self.is_full():
-            return INVALID_RID, INVALID_SLOT_NUM
-        slot_num = self.available_chunks.pop()
-        for ind in range(self.num_cols):
-            if columns[ind] != None:
-                self.bufferpool.insert_page(self.page_ids[ind], slot_num, columns[ind])
-        new_rid = self.rids.pop()
-        return new_rid, slot_num
+        with LogicalPage.logical_page_lock:
+            if self.is_full():
+                return INVALID_RID, INVALID_SLOT_NUM
+            slot_num = self.available_chunks.pop()
+            for ind in range(self.num_cols):
+                if columns[ind] != None:
+                    self.bufferpool.insert_page(self.page_ids[ind], slot_num, columns[ind])
+            new_rid = self.rids.pop()
+            return new_rid, slot_num
 
     def get_column_of_record(self, column_index: int, slot_num: int) -> int:
-        assert self.__is_valid_column_index(column_index)
-        page_id = self.page_ids[column_index]
-        phys_page = self.bufferpool.get_page(page_id)
-        return phys_page.get_column_value(slot_num)
+        with LogicalPage.logical_page_lock:
+            assert self.__is_valid_column_index(column_index)
+            page_id = self.page_ids[column_index]
+            phys_page = self.bufferpool.get_page(page_id)
+            return phys_page.get_column_value(slot_num)
 
     def update_indir_of_record(self, new_value: int, slot_num: int) -> bool:
-        page_id = self.page_ids[INDIRECTION_COLUMN]
-        phys_page = self.bufferpool.get_page(page_id)
-        return phys_page.insert_value(new_value, slot_num)
+        with LogicalPage.logical_page_lock:
+            page_id = self.page_ids[INDIRECTION_COLUMN]
+            phys_page = self.bufferpool.get_page(page_id)
+            return phys_page.insert_value(new_value, slot_num)
 
     def is_full(self) -> bool:
         return len(self.available_chunks) == 0
@@ -60,7 +65,7 @@ class BasePage(LogicalPage):
         self.tps = 0
         super().__init__(table_name, num_cols, bufferpool)
 
-    def copy_table_data_cols(self):
+    def _copy_table_data_cols(self):
         self.merge_iteration += 1
         for col in range(self.num_cols - 2):
             old_page_id = self.page_ids[col]
@@ -74,20 +79,21 @@ class BasePage(LogicalPage):
     """ Must only be called by merge """
 
     def update_record(self, columns: list, slot_num: int) -> None:
-        for ind in range(self.num_cols - 2):
-            success = self.bufferpool.insert_page(self.page_ids[ind], slot_num, columns[ind])
-            if not success:
-                # todo: make atomic by reverting all previous inserts?
-                return False
-        return True
-
+        with LogicalPage.logical_page_lock:
+            for ind in range(self.num_cols - 2):
+                success = self.bufferpool.insert_page(self.page_ids[ind], slot_num, columns[ind])
+                if not success:
+                    # todo: make atomic by reverting all previous inserts?
+                    return False
+            return True
 
 def get_copy_of_base_page(base_page: BasePage) -> BasePage:
-    copy_base_page = copy(base_page)
-    copy_base_page.available_chunks = deepcopy(base_page.available_chunks)
-    copy_base_page.page_ids = deepcopy(base_page.page_ids)
-    copy_base_page.copy_table_data_cols()
-    return copy_base_page
+    with LogicalPage.logical_page_lock:
+        copy_base_page = copy(base_page)
+        copy_base_page.available_chunks = deepcopy(base_page.available_chunks)
+        copy_base_page.page_ids = deepcopy(base_page.page_ids)
+        copy_base_page._copy_table_data_cols()
+        return copy_base_page
 
 
 class TailPage(LogicalPage):
