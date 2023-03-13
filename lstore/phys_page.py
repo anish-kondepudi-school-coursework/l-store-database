9516a0528ff3873f64408759969625df8c9bcf3d
Anish Kondepudi <anish.kondepudi@gmail.com>
2023-03-13 16:30:40 -0700

    Reverting previous locking logic

--- lstore/phys_page.py
+++ lstore/phys_page.py
@@ -1,9 +1,13 @@
 from .config import (
     PHYSICAL_PAGE_SIZE,
-    ATTRIBUTE_SIZE
+    ATTRIBUTE_SIZE,
+    INDIRECTION_COLUMN,
+    SCHEMA_ENCODING_COLUMN,
+    INVALID_SLOT_NUM,
+    INVALID_RID,
 )
-from threading import Lock
 import time
+
 
 class PhysicalPage:
     max_number_of_records: int = PHYSICAL_PAGE_SIZE // ATTRIBUTE_SIZE
@@ -17,7 +21,6 @@ class PhysicalPage:
         self.pinned: int = 0
         self.dirty: bool = False
         self.timestamp: float = time.time()
-        self.locks: list[Lock] = [Lock() for _ in range(PhysicalPage.max_number_of_records)]
 
     def get_data(self) -> bytearray:
         return self.data
@@ -42,21 +45,19 @@ class PhysicalPage:
 
     def get_column_value(self, slot_num: int) -> int:
         assert self.__is_slot_num_valid(slot_num)
-        with self.locks[slot_num]:
-            column_bytes = self.data[slot_num * ATTRIBUTE_SIZE : slot_num * ATTRIBUTE_SIZE + ATTRIBUTE_SIZE]
-            column_value = int.from_bytes(column_bytes, byteorder="big", signed=True)
-            self.timestamp = time.time()
-            return column_value
+        column_bytes = self.data[slot_num * ATTRIBUTE_SIZE : slot_num * ATTRIBUTE_SIZE + ATTRIBUTE_SIZE]
+        column_value = int.from_bytes(column_bytes, byteorder="big", signed=True)
+        self.timestamp = time.time()
+        return column_value
 
     def insert_value(self, value: int, slot_num: int) -> bool:
         if not self.__is_slot_num_valid(slot_num):
             return False
-        with self.locks[slot_num]:
-            value_bytes = value.to_bytes(ATTRIBUTE_SIZE, byteorder="big", signed=True)
-            self.data[slot_num * ATTRIBUTE_SIZE : slot_num * ATTRIBUTE_SIZE + ATTRIBUTE_SIZE] = value_bytes
-            self.dirty = True
-            self.timestamp = time.time()
-            return True
+        value_bytes = value.to_bytes(ATTRIBUTE_SIZE, byteorder="big", signed=True)
+        self.data[slot_num * ATTRIBUTE_SIZE : slot_num * ATTRIBUTE_SIZE + ATTRIBUTE_SIZE] = value_bytes
+        self.dirty = True
+        self.timestamp = time.time()
+        return True
 
     def __is_slot_num_valid(self, slot_num: int) -> bool:
         return 0 <= slot_num < PhysicalPage.max_number_of_records
